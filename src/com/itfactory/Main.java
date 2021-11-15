8d7b8c25656f1e1273ab3be5016ada2771792691
Dragos Roban <dragos.roban.training@gmail.com>
2021-11-15 20:21:59 +0200

    Reparatie Auto

--- src/com/itfactory/Main.java
+++ src/com/itfactory/Main.java
@@ -1,14 +1,52 @@
-package com.itfactory;
+package com.ItFactory;
 
-import com.itfactory.electronice.Laptop;
-import com.itfactory.servicii.Geometrie;
+import com.ItFactory.componente.CutieDeViteze;
+import com.ItFactory.componente.Motor;
+
 
 public class Main {
 
     public static void main(String[] args) {
-        Geometrie geometrie = new Geometrie();
-        System.out.println(geometrie.ariaPatratului(4));
+        Motor motor = new Motor("Volvo", 235, 2300.0);
+        CutieDeViteze cutieDeViteze = new CutieDeViteze("Volvo", 800, true);
 
-        Laptop laptop = new Laptop("Asus", "gri", 15.6);
+        Reparatie reparatieVolvo = new Reparatie("Volvo", 1200, motor, cutieDeViteze);
+        double cost = reparatieVolvo.calculeazaCostReparatie();
+        System.out.println(cost);
+
+
+//        Reparatie reparatieVolvo = new Reparatie("Volvo", 1200);
+//        Motor motor = new Motor("Fiat", 235, 2300.0);
+//        CutieDeViteze cutieDeViteze = new CutieDeViteze("Volvo", 800, true);
+//
+//
+//        reparatieVolvo.setMotor(motor);
+//        reparatieVolvo.setCutieDeViteze(cutieDeViteze);
+//
+//        System.out.println("Marca masinii este: " + reparatieVolvo.getMarcaProductie());
+//        System.out.println("Marca motorului este: " + reparatieVolvo.getMotor().getMarcaProductie());
+//        System.out.println("Marca Cutiei de viteze este: " + reparatieVolvo.getCutieDeViteze().getMarcaProductie());
+//
+//        if (reparatieVolvo.egalitateMarcaMotor == reparatieVolvo.egalitateMarcaCutieDeViteze) {
+//
+//            if (cutieDeViteze.isAutomata()) {
+//                reparatieVolvo.setCostReparatie(reparatieVolvo.getCostReparatie() + 2150.75);
+//                System.out.println("Costul reparatiei este: " + reparatieVolvo.getCostReparatie());
+//            } else {
+//                System.out.println("Costul reparatiei este: " + reparatieVolvo.getCostReparatie());
+//            }
+//        } else {
+//            System.out.println("Costul de productie nu poate fi stabilit. Componente gresite");
+//        }
     }
 }
+
+
+
+
+
+
+
+
+
+
